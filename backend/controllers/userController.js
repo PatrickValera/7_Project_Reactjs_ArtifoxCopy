815d632f737e3e3188a07c4c1a9d768c7bc32e59
saish0 <patrickvalera50@yahoo.com>
2021-10-13 21:10:04 -0700

    backend user route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
 import User from '../models/userModel.js'
-import colors from 'colors'
+
 const registerUser=async(req,res)=>{
     const {name,email,password}=req.body
     const userExists=await User.findOne({email})
     if(userExists){
-        console.log(userExists)
+        // console.log(userExists)
         res.send("USER EXISTS")
     }else{
         const user = User.create({
@@ -14,8 +14,6 @@ const registerUser=async(req,res)=>{
         })
         res.send("USER CREATED")
     }
-    // console.log("register")
-    // res.send("TEST")
 }
 
 const authenUser=async(req,res)=>{
